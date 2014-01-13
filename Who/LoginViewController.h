9ea77c282bdd21bcde5f66919e98762f91eeb882
Giancarlo Anemone <ganemone@gmail.com>
2014-01-13 18:41:22 -0500

    added dependencies folder

---
+++ Who/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Who
+//
+//  Created by Giancarlo Anemone on 1/13/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
