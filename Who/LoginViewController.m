4c1d648cb0bb9c9e61d780b731ba12993f104a31
Giancarlo Anemone <ganemone@gmail.com>
2014-01-13 19:16:54 -0500

    removed dependencies folder

---
+++ Who/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  Who
+//
+//  Created by Giancarlo Anemone on 1/13/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
