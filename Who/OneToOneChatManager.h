ba53ddd04c65e923d3c6d0a2895fba0af5f5835c
Giancarlo Anemone <ganemone@gmail.com>
2014-01-25 15:33:49 -0500

    added OneToOneChatManager

---
+++ Who/OneToOneChatManager.h
@@ -0,0 +1,15 @@
+//
+//  OneToOneChatManager.h
+//  Who
+//
+//  Created by Giancarlo Anemone on 1/25/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OneToOneChatManager : NSObject
+
+
+
+@end
---
+++ Who/OneToOneChatManager.m
@@ -0,0 +1,13 @@
+//
+//  OneToOneChatManager.m
+//  Who
+//
+//  Created by Giancarlo Anemone on 1/25/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import "OneToOneChatManager.h"
+
+@implementation OneToOneChatManager
+
+@end
