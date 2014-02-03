5b43a78762a3191120a473a456e1172731147f4e
Giancarlo Anemone <ganemone@gmail.com>
2014-02-03 10:04:36 -0500

    fixed friends tab

--- Who/ContactsViewController.m
+++ Who/ContactsViewController.m
@@ -16,43 +16,8 @@
 
 @interface ContactsViewController()
 @property (strong, nonatomic) ConnectionProvider* cp;
-@property (strong, nonatomic) NSArray *accepted;
-@property (strong, nonatomic) NSArray *pending;
 @end
 
 @implementation ContactsViewController
-
--(void)viewDidLoad{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleGetRosterPacketReceived:) name:PACKET_ID_GET_ROSTER object:nil];
-    self.cp = [ConnectionProvider getInstance];
-    [[self.cp getConnection] sendElement:[IQPacketManager createGetRosterPacket]];
-}
-
--(void)handleGetRosterPacketReceived: (NSNotification*) notification{
-    NSLog(@"Received Notification");
-    NSDictionary *data = notification.userInfo;
-    NSMutableArray *pending = [data objectForKey:USER_STATUS_PENDING];
-    NSMutableArray *accepted =[data objectForKey:USER_STATUS_FRIENDS];
-    self.accepted = accepted;
-    self.pending = pending;
-    NSLog(@"I got handleGet");
-    [self.tableView reloadData];
-}
-
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID_FRIENDS_PROTOTYPE];
-    UserProfile *currentItem = [self.accepted objectAtIndex:indexPath.row];
-    cell.textLabel.text = currentItem.jid;
-    return cell;
-}
-
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.accepted count];
-}
-
 
 @end
--- Who/FriendsViewController.m
+++ Who/FriendsViewController.m
@@ -9,22 +9,49 @@
 #import "FriendsViewController.h"
 #import "ConnectionProvider.h"
 #import "IQPacketManager.h"
+#import "Constants.h"
+#import "ConnectionProvider.h"
+#import "IQPacketManager.h"
+#import "UserProfile.h"
 
 @interface FriendsViewController()
 @property (strong, nonatomic) ConnectionProvider* cp;
+@property (strong, nonatomic) NSArray *accepted;
+@property (strong, nonatomic) NSArray *pending;
 @end
 
 @implementation FriendsViewController
 
 -(void)viewDidLoad{
-
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleGetRosterPacketReceived:) name:PACKET_ID_GET_ROSTER object:nil];
     self.cp = [ConnectionProvider getInstance];
-    DDXMLElement *iq = [IQPacketManager createGetRosterPacket];
-    NSLog(@"RosterIQ Packet: %@", iq.XMLString);
-    [[self.cp getConnection] sendElement:iq];
-    
-
+    [[self.cp getConnection] sendElement:[IQPacketManager createGetRosterPacket]];
 }
 
+-(void)handleGetRosterPacketReceived: (NSNotification*) notification{
+    NSLog(@"Received Notification");
+    NSDictionary *data = notification.userInfo;
+    NSMutableArray *pending = [data objectForKey:USER_STATUS_PENDING];
+    NSMutableArray *accepted =[data objectForKey:USER_STATUS_FRIENDS];
+    self.accepted = accepted;
+    self.pending = pending;
+    NSLog(@"I got handleGet");
+    [self.tableView reloadData];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID_FRIENDS_PROTOTYPE];
+    UserProfile *currentItem = [self.accepted objectAtIndex:indexPath.row];
+    cell.textLabel.text = currentItem.jid;
+    return cell;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.accepted count];
+}
 
 @end
