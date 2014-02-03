ead56020a988b9ddcfaa138a04f7133d34822f52
Giancarlo Anemone <ganemone@gmail.com>
2014-02-02 19:22:10 -0500

    added get messages header

--- Who/AppDelegate.h
+++ Who/AppDelegate.h
@@ -16,6 +16,9 @@
 @property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator ;
 
 -(NSArray*)getFriends;
+
+-(NSArray*)getMessages;
+
 - (void)insertMessage: (NSString*)group_id image_link:(NSString*)image_link message_body:(NSString*)message_body message_id:(NSInteger*) message_id reciever_id:(NSInteger*)reciever_id sender_id: (NSInteger*) sender_id time: (NSString*) time;
 
 @end
