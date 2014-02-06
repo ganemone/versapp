009dc87e935797cdd22b9b5c52ad3d15db481262
Giancarlo Anemone <ganemone@gmail.com>
2014-02-06 18:54:46 -0500

    working group creation and invitation.  bug with creating group id

---
+++ Who/LoadingDialogManager.h
@@ -0,0 +1,13 @@
+//
+//  LoadingDialogManager.h
+//  Who
+//
+//  Created by Giancarlo Anemone on 2/6/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoadingDialogManager : NSObject
+
+@end
---
+++ Who/LoadingDialogManager.m
@@ -0,0 +1,13 @@
+//
+//  LoadingDialogManager.m
+//  Who
+//
+//  Created by Giancarlo Anemone on 2/6/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import "LoadingDialogManager.h"
+
+@implementation LoadingDialogManager
+
+@end
