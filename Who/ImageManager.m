3d75b6ad7f7c6fb57c20e7a542606cd7ea10e753
Giancarlo Anemone <ganemone@gmail.com>
2014-02-14 20:38:00 -0500

    working image uploading

--- Who/ImageManager.m
+++ Who/ImageManager.m
@@ -53,9 +53,10 @@ NSString *const DICTIONARY_KEY_IMAGE_URL = @"dictionary_key_downloaded_url";
     
     AppDelegate *delegate = [UIApplication sharedApplication].delegate;
     NSString *postString = [NSString stringWithFormat:@"username=%@&session=%@&method=%@&image=%@", [ConnectionProvider getUser], delegate.sessionID, @"message", encodedImageString];
+    postString = [postString stringByReplacingOccurrencesOfString:@"+" withString:@"%2B"];
     //NSLog(@"Image: %@", encodedImageString);
     NSData *postData = [NSData dataWithBytes:[postString UTF8String] length:[postString length]];
-    [uploadRequest setValue:[NSString stringWithFormat:@"%d", postString.length] forHTTPHeaderField:@"Content-Length"];
+    [uploadRequest setValue:[NSString stringWithFormat:@"%lu", (unsigned long)postString.length] forHTTPHeaderField:@"Content-Length"];
     [uploadRequest setHTTPBody:postData];
 
     // create connection and set delegate if needed
@@ -96,119 +97,4 @@ NSString *const DICTIONARY_KEY_IMAGE_URL = @"dictionary_key_downloaded_url";
     NSLog(@"Did receive NSURL Response: %@", response);
 }
 
-+ (NSData *)base64DataFromString: (NSString *)string
-{
-    unsigned long ixtext, lentext;
-    unsigned char ch, inbuf[4], outbuf[3];
-    short i, ixinbuf;
-    Boolean flignore, flendtext = false;
-    const unsigned char *tempcstring;
-    NSMutableData *theData;
-    
-    if (string == nil)
-    {
-        return [NSData data];
-    }
-    
-    ixtext = 0;
-    
-    tempcstring = (const unsigned char *)[string UTF8String];
-    
-    lentext = [string length];
-    
-    theData = [NSMutableData dataWithCapacity: lentext];
-    
-    ixinbuf = 0;
-    
-    while (true)
-    {
-        if (ixtext >= lentext)
-        {
-            break;
-        }
-        
-        ch = tempcstring [ixtext++];
-        
-        flignore = false;
-        
-        if ((ch >= 'A') && (ch <= 'Z'))
-        {
-            ch = ch - 'A';
-        }
-        else if ((ch >= 'a') && (ch <= 'z'))
-        {
-            ch = ch - 'a' + 26;
-        }
-        else if ((ch >= '0') && (ch <= '9'))
-        {
-            ch = ch - '0' + 52;
-        }
-        else if (ch == '+')
-        {
-            ch = 62;
-        }
-        else if (ch == '=')
-        {
-            flendtext = true;
-        }
-        else if (ch == '/')
-        {
-            ch = 63;
-        }
-        else
-        {
-            flignore = true;
-        }
-        
-        if (!flignore)
-        {
-            short ctcharsinbuf = 3;
-            Boolean flbreak = false;
-            
-            if (flendtext)
-            {
-                if (ixinbuf == 0)
-                {
-                    break;
-                }
-                
-                if ((ixinbuf == 1) || (ixinbuf == 2))
-                {
-                    ctcharsinbuf = 1;
-                }
-                else
-                {
-                    ctcharsinbuf = 2;
-                }
-                
-                ixinbuf = 3;
-                
-                flbreak = true;
-            }
-            
-            inbuf [ixinbuf++] = ch;
-            
-            if (ixinbuf == 4)
-            {
-                ixinbuf = 0;
-                
-                outbuf[0] = (inbuf[0] << 2) | ((inbuf[1] & 0x30) >> 4);
-                outbuf[1] = ((inbuf[1] & 0x0F) << 4) | ((inbuf[2] & 0x3C) >> 2);
-                outbuf[2] = ((inbuf[2] & 0x03) << 6) | (inbuf[3] & 0x3F);
-                
-                for (i = 0; i < ctcharsinbuf; i++)
-                {
-                    [theData appendBytes: &outbuf[i] length: 1];
-                }
-            }
-            
-            if (flbreak)
-            {
-                break;
-            }
-        }
-    }
-    
-    return theData;
-}
 @end
