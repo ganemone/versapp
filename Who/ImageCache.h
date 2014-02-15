1973503df4cb3457a2880476abfc611e66d0227b
Giancarlo Anemone <ganemone@gmail.com>
2014-02-15 11:06:05 -0500

    added image cache class

---
+++ Who/ImageCache.h
@@ -0,0 +1,13 @@
+//
+//  ImageCache.h
+//  Who
+//
+//  Created by Giancarlo Anemone on 2/15/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageCache : NSObject
+
+@end
---
+++ Who/ImageCache.m
@@ -0,0 +1,13 @@
+//
+//  ImageCache.m
+//  Who
+//
+//  Created by Giancarlo Anemone on 2/15/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import "ImageCache.h"
+
+@implementation ImageCache
+
+@end
