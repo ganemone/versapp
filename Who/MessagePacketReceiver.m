222b30db7beaeef8bf37894472d83088e7a5e657
Giancarlo Anemone <ganemone@gmail.com>
2014-03-25 18:59:14 -0400

    added new pictures

--- Who/MessagePacketReceiver.m
+++ Who/MessagePacketReceiver.m
@@ -104,6 +104,8 @@
             }
             NSDictionary *messageDictionary = [NSDictionary dictionaryWithObject:message.thread forKey:MESSAGE_PROPERTY_GROUP_ID];
             if ([ChatDBManager hasChatWithID:message.thread] == NO) {
+                NSLog(@"Getting New Chat HERE!!");
+                NSLog(@"Chat Name should be anonymous friend");
                 [ChatDBManager insertChatWithID:message.thread chatName:@"Anonymous Friend" chatType:CHAT_TYPE_ONE_TO_ONE participantString:senderID status:STATUS_JOINED];
             }
             [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_ONE_TO_ONE_MESSAGE_RECEIVED object:nil userInfo:messageDictionary];
