e64486fc84ad16ca426cc43cf3f979c78c5d6600
Giancarlo Anemone <ganemone@gmail.com>
2014-03-26 19:41:06 -0400

    finished speeding up confession table cell loading

--- Who/Confession.h
+++ Who/Confession.h
@@ -18,13 +18,14 @@
 @property (strong, nonatomic) NSMutableArray *favoritedUsers;
 @property CGFloat height;
 @property CGRect cellFrame;
-@property CGRect textFrame;
-@property CGRect footerFrame;
-@property CGRect chatButtonFrame;
-@property CGRect chatLabelFrame;
-@property CGRect favoriteButtonFrame;
-@property CGRect favoriteLabelFrame;
-@property CGRect timestampLabelFrame;
+@property (strong, nonatomic) UITextView *textView;
+@property (strong, nonatomic) UIImageView *footerView;
+@property (strong, nonatomic) UIButton *chatButton;
+@property (strong, nonatomic) UIButton *favoriteButton;
+@property (strong, nonatomic) UILabel *chatLabel;
+@property (strong, nonatomic) UILabel *favoriteLabel;
+@property (strong, nonatomic) UILabel *timestampLabel;
+
 @property BOOL hasCalculatedFrames;
 
 +(instancetype)create:(NSString*)body imageURL:(NSString*)imageURL;
