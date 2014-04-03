4d615be0552183220ee5278371ac1e891875fbad
Riley Lundquist <rlundquist3@gmail.com>
2014-04-03 13:24:17 -0400

    Took + signs out of confession chat names

--- Who/OneToOneConversationViewController.m
+++ Who/OneToOneConversationViewController.m
@@ -37,7 +37,7 @@
     if ([[ConnectionProvider getUser] compare:[self.chatMO.chat_id substringToIndex:[[ConnectionProvider getUser] length]]] != 0) {
         [self.headerLabel setText:ANONYMOUS_FRIEND];
     } else {
-        [self.headerLabel setText:[self.chatMO getChatName]];
+        [self.headerLabel setText:[[self.chatMO getChatName] stringByReplacingOccurrencesOfString:@"+" withString:@" "]];
     }
     
     [self.headerLabel setFont:[StyleManager getFontStyleMediumSizeXL]];
