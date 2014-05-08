ff6abaa9ae2c3cd471282c94d69a35f129937725
Giancarlo Anemone <ganemone@gmail.com>
2014-05-08 11:10:34 -0400

    added security tutorial view controller

---
+++ Who/SecurityTutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecurityTutorialViewController.h
+//  Versapp
+//
+//  Created by Giancarlo Anemone on 5/8/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecurityTutorialViewController : UIViewController
+
+@end
---
+++ Who/SecurityTutorialViewController.m
@@ -0,0 +1,43 @@
+//
+//  SecurityTutorialViewController.m
+//  Versapp
+//
+//  Created by Giancarlo Anemone on 5/8/14.
+//  Copyright (c) 2014 Giancarlo Anemone. All rights reserved.
+//
+
+#import "SecurityTutorialViewController.h"
+#import "StyleManager.h"
+
+@interface SecurityTutorialViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+
+@end
+
+@implementation SecurityTutorialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.titleLabel setFont:[StyleManager getFontStyleLightSizeTitle]];
+    [self.descriptionTextView setFont:[StyleManager getFontStyleLightSizeLarge]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
